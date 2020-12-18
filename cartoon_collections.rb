40f4d91e0e11ef120cb7bee05f5e27ebd663b4e3
Reynelle <Rtalford13@gmail.com>
2020-12-18 04:43:21 -0500

    commit tree 44c6456706b36e2a73a905669686e0ced2a671a9

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(array)# code an argument here
+  array.each_with_index { |array, index| puts "#{index+1} #{array}" }
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+    planeteer_calls.collect{|planeteer_calls|planeteer_calls.capitalize+"!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+   array.any? do |array|
+      array.length > 4     
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)
+  
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find{|c| cheese_types.include? (c)}
 end
+
